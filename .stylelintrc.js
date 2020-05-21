0f6ff6f79346539b22f9a6c85fcf5ff4669ef77f
senntyou <jiangjinbelief@163.com>
2020-05-21 09:22:37 +0800

    init

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'unit-no-unknown': null,
+    'comment-empty-line-before': null,
+    'length-zero-no-unit': null,
+    'color-hex-length': null,
+    'property-no-unknown': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'selector-pseudo-element-colon-notation': null,
+    'declaration-block-no-duplicate-properties': null,
+    'no-empty-source': null,
+    'selector-type-no-unknown': null,
+  },
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# uni-app-starter
+
+[uni-app](https://github.com/dcloudio/uni-app) 项目开发基础脚手架
+
+## 用法
+
+按照官方指南创建好 `uni-app` 项目之后，把这个项目的所有文件复制过去。
+
+如果有文件已经存在，可以按照实际情况保留某一方文件或者合并、调整内容为一个文件。
